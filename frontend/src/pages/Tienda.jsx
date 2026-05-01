7c282d166adb8b2c25c2f1c9a2942716331ff553
Lucafagnani1 <lucafagnani5@gmail.com>
2026-05-01 03:50:41 -0300

    URLs produccion

--- frontend/src/pages/Tienda.jsx
+++ frontend/src/pages/Tienda.jsx
@@ -17,7 +17,7 @@ function Tienda() {
   const [cargando, setCargando] = useState(true)
 
   useEffect(() => {
-    fetch('https://kiosco-app-production-5cff.up.railway.app/api/productos')
+    fetch(`${API_URL}/api/productos`)
       .then(res => res.json())
       .then(data => {
         setProductos(data)
