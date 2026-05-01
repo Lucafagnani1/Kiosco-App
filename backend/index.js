3bc402a4141b567a5d40cd408555c88084a14a7d
Lucafagnani1 <lucafagnani5@gmail.com>
2026-05-01 03:34:48 -0300

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,12 @@ const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
-app.use(cors())
+
+app.use(cors({
+  origin: '*'
+}))
 app.use(express.json())
 
-// Rutas
 const productosRouter = require('./routes/productos')
 app.use('/api/productos', productosRouter)
 
