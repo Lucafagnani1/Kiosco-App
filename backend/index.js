2a5fd4560dda4aa2363113029426084429a7406f
Lucafagnani1 <lucafagnani5@gmail.com>
2026-05-01 03:44:17 -0300

    fix cors headers

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,14 @@ require('dotenv').config()
 
 const app = express()
 
-app.use(cors({
-  origin: '*'
-}))
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  if (req.method === 'OPTIONS') return res.sendStatus(200)
+  next()
+})
+
 app.use(express.json())
 
 const productosRouter = require('./routes/productos')
